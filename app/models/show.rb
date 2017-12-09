45d690044c8a389dae353d6aa9cdb1d19ceb398b
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-12-09 02:09:42 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
-  
+  attr_accessor :genre
+
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def build_network(hash)
+    network = Network.new(hash)
+    #network.shows << self
+    self.network = network
+  end
+
 end
